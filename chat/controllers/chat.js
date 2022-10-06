c19da356d862ffb6a62619312805c4198e291927
SoniSuciadi <sonisuciadi@gmail.com>
2022-10-06 16:44:55 +0700

    delete migration

--- chat/controllers/chat.js
+++ chat/controllers/chat.js
@@ -1,23 +1,23 @@
 const mChat = require("../models/chat");
-const { user } = require("../models");
 
 class Chat {
   static async getChats(req, res, next) {
     try {
       let result = await mChat.find().exec();
-      console.log(result);
-      let allUser = await user.findAll({ raw: true });
 
-      let chats = result.map((el) => {
-        return {
-          message: el.message,
-          imgUrl: el.imgUrl,
-          User: allUser.find((element) => element.id == el.sender),
-          createdAt: el.createdAt,
-        };
-      });
+      // orchestrator
+      // let allUser = await user.findAll({ raw: true });
 
-      res.status(200).json(chats);
+      // let chats = result.map((el) => {
+      //   return {
+      //     message: el.message,
+      //     imgUrl: el.imgUrl,
+      //     User: allUser.find((element) => element.id == el.sender),
+      //     createdAt: el.createdAt,
+      //   };
+      // });
+
+      res.status(200).json(result);
     } catch (error) {
       next(error);
     }
