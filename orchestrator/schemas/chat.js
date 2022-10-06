f2439d6b239318b1ed2eb71f1e1aa9001ff304ef
SoniSuciadi <sonisuciadi@gmail.com>
2022-10-06 20:58:17 +0700

    orchestrator chat done

--- orchestrator/schemas/chat.js
+++ orchestrator/schemas/chat.js
@@ -1,7 +1,72 @@
-const { gql } = require("apollo-server");
-const { apiApp } = require("../configs/axios");
+const { gql, AuthenticationError } = require("apollo-server");
+const { apiChat } = require("../configs/axios");
 const redis = require("../configs/redis");
 
-const type=gql`
-    type message
-`
+const typeDefs = gql`
+  input Chat {
+    imgUrl: String
+    message: String
+  }
+  type User {
+    id: ID
+    name: String
+  }
+  type post {
+    message: String
+  }
+
+  type chats {
+    _id: ID
+    text: String
+    image: String
+    user: User
+    createdAt: String
+  }
+
+  type Query {
+    getUser: User
+  }
+
+  type Mutation {
+    getChats: [chats]
+    postChats(newChat: Chat): post
+  }
+`;
+const resolvers = {
+  Mutation: {
+    getChats: async (parent, args, contex, info) => {
+      try {
+        if (!contex.authScope) throw new AuthenticationError("Unauthorized");
+        const chat = await redis.get("chat:getChats");
+        if (!chat) {
+          let { data } = await apiChat.get("/chats", {
+            headers: { access_token: contex.autScope },
+          });
+          redis.set("chat:getChats", JSON.stringify(data));
+          return data;
+        }
+        return JSON.parse(chat);
+      } catch (error) {
+        return error;
+      }
+    },
+    postChats: async (_, { newChat }, contex) => {
+      try {
+        const { imgUrl, message } = newChat;
+        if (!contex.authScope) throw new AuthenticationError("Unauthorized");
+        let { data } = await apiChat.post(
+          "/chats",
+          { imgUrl, message },
+          {
+            headers: { access_token: contex.authScope },
+          }
+        );
+        return data;
+      } catch (error) {
+        return error;
+      }
+    },
+  },
+};
+
+module.exports = { resolvers, typeDefs };
