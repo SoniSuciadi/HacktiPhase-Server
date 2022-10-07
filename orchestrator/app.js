621a86112579790a20834019ed17f3840f59ece5
nuninnih <nunin.fzu@gmail.com>
2022-10-07 09:07:19 +0700

    orchestra-user

--- orchestrator/app.js
+++ orchestrator/app.js
@@ -1,4 +1,25 @@
 const { ApolloServer } = require("apollo-server");
+const userSchema = require("./schemas/userSchema");
+
+const server = new ApolloServer({
+  typeDefs: [
+    // appSchema.typeDefs,
+    userSchema.typeDefs,
+    // forumSchema.typeDefs,
+    // chatSchema.typeDefs,
+  ],
+  resolvers: [
+    // appSchema.resolvers,
+    userSchema.resolvers,
+    // forumSchema.resolvers,
+    // chatSchema.resolvers,
+  ],
+  introspection: true,
+  playground: true,
+  context: ({ req }) => ({
+    authScope: req.headers.access_token,
+  }),
+});
 
 server.listen(process.env.PORT || 4000).then(({ url }) => {
   console.log("server ready at ", url);
