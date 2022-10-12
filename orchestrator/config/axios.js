04fc356e4e38f09292c1b76c781e2318368fca90
SoniSuciadi <sonisuciadi@gmail.com>
2022-10-13 00:58:11 +0700

    fix bug forum

--- orchestrator/config/axios.js
+++ orchestrator/config/axios.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 const apiChat = axios.create({
-  // baseURL: "https://hacktiphase-chat.herokuapp.com",
-  baseURL: "http://localhost:3003/",
+  baseURL: "https://hacktiphase-chat.herokuapp.com",
 });
 
 module.exports = { apiChat };
